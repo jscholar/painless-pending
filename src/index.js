de69fc3a21795215f471d1424f0a25050796e0dc
jscholar <jinruncai@gmail.com>
2019-01-26 18:59:43 -0800

    Implementing state object to store filters

--- src/index.js
+++ src/index.js
@@ -3,14 +3,33 @@
  */
 
 import './css/style.css'
-import { elements, readInput, copyResults, displayResults, initUI } from './js/UIcontroller'
+import { currentJulDay } from './js/models/Templates'
+import { elements, readInput, copyResults, displayResults, initUI, invalidInput, getJulianInput, formatJul } from './js/UIcontroller'
 import { buildPending } from './js/parser'
 import { updatePending } from './js/updater'
 
+let state = {
+    julRange: {
+        begin: null,
+        end: null,
+        days: null
+    },
+    filters: [
+        {
+            keyword: null,
+            color: '#DDDDDD'
+        }
+    ]
+}
+
 /*  Setup event handlers */
  {
-     init();
-     elements.readOldButton.addEventListener('click', processText)
+     elements.updatePendingBtn.addEventListener('click', processText);
+
+     /** Additions, removals, or changes to any filter are bubbled up to filters element */
+     elements.julFilters.addEventListener('change', e => updateJulianRange(e));
+
+     window.addEventListener("load", init);
  }
 
 function processText() {
@@ -20,18 +39,27 @@ function processText() {
     let [oldText, newText] = readInput();
 
     if (oldText.length && newText.length) {
+
         /* Get Pendings object */
         previousPending = buildPending(oldText, 'previous');
         currentPending = buildPending(newText, 'current');
-            /* Update current pendings with previous resolutions */
+
+        /* Update current pendings with previous resolutions */
         updatePending(previousPending, currentPending);
-        console.log(currentPending);
-        displayResults(currentPending);
+
+
+        displayResults(state, currentPending);
     } else {
-        console.log('no input received');
+        invalidInput();
     }
 }
 
+function updateJulianRange(e) {
+    state.julRange = {...getJulianInput()};
+    formatJul(e.srcElement);
+}
+
 function init() {
-    initUI();
+    let currJul = currentJulDay();
+    initUI(currJul);
 }
