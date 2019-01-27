748fa3daacb237b59a88f55c4fa21530a0bb61e0
jscholar <jinruncai@gmail.com>
2019-01-27 15:20:11 -0800

    Added filter for unresolved

--- src/js/specHandler.js
+++ src/js/specHandler.js
@@ -4,11 +4,11 @@
 
 import { genPendLine } from './models/Templates'
 
-export default function specHandler(spec, julRange) {
+export default function specHandler(spec, julRange, filters) {
     if (!julInRange(spec.jul, julRange.begin, julRange.end)) {
         return null;
     }
-    return createElement(spec, julRange);
+    return createElement(spec, filters);
 }
 
 const julInRange = (jul, julBegin, julEnd) => {
@@ -21,13 +21,16 @@ const julInRange = (jul, julBegin, julEnd) => {
     }
 }
 
-const createElement = (spec, rules) => {
+const createElement = (spec, filters) => {
     let newLine = document.createElement("span");
 
     let newText = document.createTextNode(
         genPendLine(spec.line, spec.res ? spec.res : '')
     );
     newLine.appendChild(newText);
+    if (!spec.res) {
+        newLine.style.backgroundColor = filters[0].color;
+    }
 
     return newLine;
 }
