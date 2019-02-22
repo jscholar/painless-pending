5013276b0aed1acc8de85aa642e2f0442c2a89d5
jscholar <jinruncai@gmail.com>
2019-02-21 19:03:36 -0800

    Revert "Split index styles and component styles"
    This reverts commit 3f42bf71ba863faa03d962981d8276c4be544910.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@
  * A simple web-app to assist with pending
  */
 
-
 import PendingView from './js/views/PendingView/PendingView';
 import HelpSection from './js/views/HelpView/HelpView';
 
-import Elements from './js/views/Elements';
+import Elements from './js/views/PendingView/Elements';
 
 const App = () => {
-
 
     // Setup Event Handlers
     {
